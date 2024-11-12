10175c3fda495868e79f74dffe067b9841ae0f9c
morales44 <115790097+morales44@users.noreply.github.com>
2024-11-12 17:42:31 +0100

    a

--- microservicio_Reservas/index.js
+++ microservicio_Reservas/index.js
@@ -1,57 +1,43 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = 6000
+const axios = require('axios')
+
+app.get('/restaurants/city/:city', async (req, res) => {
+  try {
+    const city = req.params.city
+    const url = `https://api.yelp.com/v3/businesses/search?term=restaurants&location=${city}`
+    
+    const response = await axios.get(url,{
+      headers: {
+        Authorization: `Bearer ${process.env.YELP_API_KEY}` // Reemplaza con tu clave de API de Yelp
+      }
+    })
+    const data = response.data
+
+    const restaurantes = data.businesses.map(restaurante => {
+      return {
+        id: restaurante.id,
+        name: restaurante.name, 
+        abierto: restaurante.is_closed,
+        categorias: restaurante.categories,
+        barrio: restaurante.location.address1,
+        ciudad: restaurante.location.city,
+        telefono: restaurante.display_phone,
+        
+      }
+    })
+
+    res.json(restaurantes)
+
+    console.log("Elementos recividos correctamente")
+  } catch (error) {
+    res.status(500).json({error: 'Error obteniendo restaurantes'})
+    console.log("Error finding restaurants in " + req.params.city + ": " + error.message)
+  }
+})
 
 app.listen(port, () => {
     console.log("Servidor escuchando en puerto " + port)
 })
-
-
-app.get('/yelp/getNegocios', async(req, res) => {
-    try {
-        const term = req.query.term;
-        const location = req.query.location;
-        const limit = req.query.limit || 10;
-
-        if (!term || !location) {
-            console.error("Es necesario proporcionar 'term' y 'location' en la URL.");
-            return;
-        }
-
-        const url = `https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&limit=${limit}`;
-
-
-    } catch (error) {
-        
-    }
-});
-    
-
-    if (!term || !location) {
-        console.error("Es necesario proporcionar 'term' y 'location' en la URL.");
-        return;
-      }
-    
-      const url = `https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&limit=${limit}`;
-      
-     
-    
-        // Verificar si la solicitud fue exitosa
-        if (!response.ok) {
-          throw new Error(`Error: ${response.statusText}`);
-        }
-    
-        // Parsear la respuesta en formato JSON
-        const data = await response.json();
-        
-        // Filtrar y parsear la información más importante de cada negocio
-        const negocios = data.businesses.map(negocio => ({
-          nombre: negocio.name,
-          calificacion: negocio.rating,
-          direccion: negocio.location.address1, // Dirección principal
-          telefono: negocio.phone,
-          url: negocio.url,
-          tipoComida: negocio.categories.map(categoria => categoria.title).join(', ') // Unir categorías en un solo string
-        }));
-    }
-}
