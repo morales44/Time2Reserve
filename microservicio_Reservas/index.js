f032f09ad893add212c2725e5e73eddff6f63e19
morales44 <115790097+morales44@users.noreply.github.com>
2024-11-13 14:22:10 +0100

    base de datos

--- microservicio_Reservas/index.js
+++ microservicio_Reservas/index.js
@@ -3,6 +3,52 @@ const express = require('express')
 const app = express()
 const port = 6000
 const axios = require('axios')
+const mongoose = require('mongoose')
+const connectDB = require('./config/db');
+
+connectDB();
+
+const RestauranteSchema = new mongoose.Schema({
+  id: String,
+  name: String, 
+  abierto: Boolean,
+  categorias: Array,
+  barrio: String,
+  ciudad: String,
+  telefono: String
+})
+
+const Restaurante = mongoose.model('Restaurante', RestauranteSchema)
+
+async function uploadData(restaurantes, ciudad) {
+  try {
+    const restaurantesBBDD = await Restaurante.find();
+
+    if (restaurantesBBDD.length > 0) {
+      for (const restaurante of restaurantesBBDD) {
+        if (restaurante['ciudad'] === ciudad) {
+          await Restaurante.deleteOne(restaurante);
+        }
+      }
+    }
+
+    const restaurantesParaInsertar = restaurantes.map(restaurante => ({
+      id: restaurante['id'],
+      name: restaurante['name'],
+      abierto: restaurante['abierto'],
+      categorias: restaurante['categorias'],
+      barrio: restaurante['barrio'],
+      ciudad: restaurante['ciudad'],
+      telefono: restaurante['telefono']
+    }))
+
+    await Restaurante.insertMany(restaurantesParaInsertar);
+
+    console.log('Restaurantes añadidos a la base de datos')
+  } catch(error){
+    console.error('Error al cargar los datos ' + error)
+  }
+}
 
 app.get('/restaurants/city/:city', async (req, res) => {
   try {
@@ -24,10 +70,11 @@ app.get('/restaurants/city/:city', async (req, res) => {
         categorias: restaurante.categories,
         barrio: restaurante.location.address1,
         ciudad: restaurante.location.city,
-        telefono: restaurante.display_phone,
-
+        telefono: restaurante.display_phone
       }
-    }) 
+    })
+
+    uploadData(restaurantes, city)
 
     res.json(restaurantes)
 
